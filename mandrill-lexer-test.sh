d68f07d7cc7998718db66c64b853a3fffa5cd5f3
croissantfish <croissantfish@outlook.com>
2025-05-08 13:41:47 +0800

    更新了用于批量测试的脚本

--- mandrill-lexer-test.sh
+++ mandrill-lexer-test.sh
@@ -49,31 +49,32 @@ for name in ${names[@]}; do
     echo "####################################################"
     _SC cd students
     if [ ! -d $name ]; then
-        _SC mkdir $name "&&" cd $name
-        (_SC git clone "git@bitbucket.org:${name}/mandrill2025.git") || { echo "[ERROR] cannot clone code, goto next student..."; continue; }
-        _SC cd ..
+        _SC mkdir $name
     fi
     if [ ! -d $name/mandrill2025 ]; then
-        echo "[ERROR] cannot find student code, goto next student..."
-        continue
+        _SC cd $name
+        (_SC git clone "git@bitbucket.org:${name}/mandrill2025.git") || { echo "[ERROR] cannot clone code, goto next student..."; echo $name "0/-8" >>$statistics; continue; }
+         _SC cd ..
     fi
     _SC cd $name/mandrill2025
-    (_SC git checkout -f main) || { echo "[ERROR] cannot checkout the main, goto next student..."; continue; }
-    (_SC git pull) || { echo "[ERROR] cannot update student code, goto next student..."; continue; }
-    _SC git fetch --tags
-    (_SC git checkout -f lexer) || { echo "[ERROR] cannot checkout lexer tag, goto next student..."; continue; }
-    (_SC make clean) || { echo "[ERROR] clean previous build failed, goto next student..."; continue; }
-    (_SC make) || { echo "[ERROR] compiling student code failed, goto next student..."; continue; }
-    (_SC cat ./lexervars.sh) || { echo "[ERROR] file to set CCHK is not existed, goto next student..."; continue; }
+    (_SC git checkout -f main) || { echo "[ERROR] cannot checkout the main, goto next student..."; echo $name "0/-7" >>$statistics; continue; }
+    (_SC git pull) || { echo "[ERROR] cannot update student code, goto next student..."; echo $name "0/-6" >>$statistics; continue; }
+    _SC git fetch -f --tags
+    (_SC git checkout -f lexer) || { echo "[ERROR] cannot checkout lexer tag, goto next student..."; echo $name "0/-5" >>$statistics; continue; }
+    (_SC make clean) || { echo "[ERROR] clean previous build failed, goto next student..."; echo $name "0/-4" >>$statistics; continue; }
+    (_SC make) || { echo "[ERROR] compiling student code failed, goto next student..."; echo $name "0/-3" >>$statistics; continue; }
+    (_SC cat ./lexervars.sh) || { echo "[ERROR] file to set CCHK is not existed, goto next student..."; echo $name "0/-2" >>$statistics; continue; }
 
     unset -v CCHK
     set -x
     source ./lexervars.sh #can't ensure SC here
     set +x
     echo CCHK=$CCHK
+    echo CCHK=$CCHK >>$log_file
 
     if [ ! -d bin ]; then
         echo "[ERROR] make did not create bin directory, goto next student..."
+        echo $name "0/-1" >>$statistics
         continue
     fi
     _SC cd bin
@@ -81,6 +82,7 @@ for name in ${names[@]}; do
 #        filein=${filec%.mds}.in
         fileout=$stdlexoutdir/$(basename ${filec%.mds}).lexerout
         _SC cp $filec data.mds
+        echo "[RUNNING] timeout $timeo $CCHK <data.mds 1>data.lexerout"
         timeout $timeo $CCHK <data.mds 1>data.lexerout
 #        if [ -f $filein ]; then
 #            timeout $timeo spim -ldata 209715200 -lstack 104857600 -stat_file spimstat -file assem.s <$filein 1>spimout 2>/dev/null
@@ -88,14 +90,15 @@ for name in ${names[@]}; do
 #            timeout $timeo spim -ldata 209715200 -lstack 104857600 -stat_file spimstat -file assem.s 1>spimout 2>/dev/null
 #        fi
         pure_file_name=$(basename $filec)
-        pure_file_name=${pure_file_name%.mx}
-        diff data.lexerout $fileout >$diff_result_dir/$pure_file_name.txt
+        pure_file_name=${pure_file_name%.mds}
+        diff data.lexerout $fileout >$diff_result_dir/$pure_file_name.lexerout.diff.txt
         full_score=$((full_score+1))
 #        file_limit=${filec%.mx}.limit
 #        if [ -f $file_limit ]; then
 #            full_score=$((full_score+1))
 #        fi
         if [ ! -s $diff_result_dir/$pure_file_name ]; then
+            echo PASSED
             score=$((score+1))
 #            if [ -f $file_limit ]; then
 #                num=-1
@@ -108,8 +111,8 @@ for name in ${names[@]}; do
 #                fi
 #            fi
         else
-            echo ${filec%.mx} : FAILED >>$log_file
-            echo FAILED
+            echo ${filec%.mds} : FAILED >>$log_file
+            echo "FAILED: WRONG ANSWER, Please check $diff_result_dir/$pure_file_name.lexerout.diff.txt."
         fi
         sleep 1
     done
